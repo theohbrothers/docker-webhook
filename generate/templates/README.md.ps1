79cd2d4e2f200ecc763ad73e37afbe156a81a227
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-10-26 15:29:32 +0000

    Docs (readme): Update readme with FAQ on common errors

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -27,9 +27,12 @@ $(
 }) -join ''
 )
 
+"@
+
+@'
 ## Usage
 
-``````sh
+```sh
 # Create hooks.yml, see: https://github.com/adnanh/webhook#configuration
 cat - > hooks.yml <<'EOF'
 - id: hello-world
@@ -37,10 +40,42 @@ cat - > hooks.yml <<'EOF'
 EOF
 
 # Start container
-docker run -it -p 9000:9000 -v `$(pwd)/hooks.yml:/config/hooks.yml:ro theohbrothers/docker-webhook
+docker run -it -p 9000:9000 -v $(pwd)/hooks.yml:/config/hooks.yml:ro theohbrothers/docker-webhook
 
 # Run the webhook
-wget -qO- "http://`$HOSTNAME:9000/hooks/hello-world"
-``````
+wget -qO- "http://$HOSTNAME:9000/hooks/hello-world"
+```
 
-"@
+## FAQ
+
+### Q: `webhook` fails with error `__nanosleep_time64: symbol not found`
+
+On Raspberry Pi, running `alpine-3.12` fails with error:
+
+```sh
+$ docker run -it theohbrothers/docker-webhook:2.8.0-alpine-3.12
+Error relocating /usr/local/bin/webhook: __nanosleep_time64: symbol not found
+```
+
+The solution is to use `alpine-3.13` or later
+
+```sh
+$ docker run -it theohbrothers/docker-webhook:2.8.0-alpine-3.13
+```
+
+### Q: `ping` fails with error `ping: clock_gettime(MONOTONIC) failed`
+
+On Raspberry Pi, running `ping` on `alpine-3.13` and above might fail with error:
+
+```sh
+$ docker run -it theohbrothers/docker-webhook:2.8.0-alpine-3.13
+PING 1.1.1.1 (1.1.1.1): 56 data bytes
+ping: clock_gettime(MONOTONIC) failed
+```
+
+The solution is to use `--security-opt seccomp=unconfined` option. See [here](https://gitlab.alpinelinux.org/alpine/aports/-/issues/12091)
+
+```sh
+$ docker run -it --security-opt seccomp=unconfined theohbrothers/docker-webhook:2.8.0-alpine-3.13
+```
+'@
