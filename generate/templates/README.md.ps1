b4bdf1293a50db4a02b486d4085e34fe00c8342e
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-10-26 12:41:38 +0000

    Docs (readme): Update readme

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -5,7 +5,7 @@
 [![github-release](https://img.shields.io/github/v/release/theohbrothers/docker-webhook?style=flat-square)](https://github.com/theohbrothers/docker-webhook/releases/)
 [![docker-image-size](https://img.shields.io/docker/image-size/theohbrothers/docker-webhook/latest)](https://hub.docker.com/r/theohbrothers/docker-webhook)
 
-Dockerized alpine with useful tools.
+Dockerized [``webhook``](https://github.com/adnanh/webhook) with useful tools.
 
 ## Tags
 
@@ -26,4 +26,21 @@ $(
     }
 }) -join ''
 )
+
+## Usage
+
+``````sh
+# Create hooks.yml, see: https://github.com/adnanh/webhook#configuration
+cat - > hooks.yml <<'EOF'
+- id: hello-world
+  execute-command: echo
+EOF
+
+# Start container
+docker run -it -p 9000:9000 -v `$(pwd)/hooks.yml:/config/hooks.yml:ro theohbrothers/docker-webhook
+
+# Run the webhook
+wget -qO- "http://`$HOSTNAME:9000/hooks/hello-world"
+``````
+
 "@
